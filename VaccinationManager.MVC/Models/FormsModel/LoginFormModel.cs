56cb0bd4b119d24f62f3923e61ce0f81376f083e
isaskou <isabel.skou@yahoo.fr>
2021-07-26 09:11:26 +0200

    update UI

---
+++ VaccinationManager.MVC/Models/FormsModel/LoginFormModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationManager.MVC.Models.FormsModel
+{
+    public class LoginFormModel
+    {
+    }
+}
